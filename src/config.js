45e486fc5fc9cd00620223966b0b98109d306bb8
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-04-05 11:00:04 -0300

    Edit config SITE

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports.SITE = {
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'Matias Vallejos - Transforming Concepts into Code',
+  title: 'Matias Vallejos - Full Stack Web Developer 🇦🇷',
   description: "From the soccer field⚽ to code💻 Argentine web developer 🇦🇷 shaping the digital future. I don't just code, I create solutions. Driving national technological Capitalism without borders.",
   keywords: 'Matias Vallejos, Web Developer, Software Engineer, Argentina, Desafio Planeta, Capitalismo Tecnologico Nacional',
 }
