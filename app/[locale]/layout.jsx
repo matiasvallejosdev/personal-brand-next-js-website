a5e231cb69f98ad2bf28938257a7f87d0395483b
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-04-05 14:15:59 -0300

    Fixing meta opengraph

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -16,7 +16,7 @@ const customFont = CustomFont({
 export const metadata = {
   title: SITE.title,
   description: SITE.description,
-  keywords: SITE.keywords
+  keywords: SITE.keywords,
 };
 
 export default function RootLayout({ children, params: { locale } }) {
@@ -25,6 +25,25 @@ export default function RootLayout({ children, params: { locale } }) {
     <html lang={locale} className={`${customFont.variable}`}>
       <head>
         <link rel="icon" href="/static/favicon.ico" />
+
+        <meta property="og:url" content="https://matiasvallejos.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Matias Vallejos - Full Stack Web Developer 🇦🇷" />
+        <meta property="og:description" content="Let's explore my personal site! 🚀" />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/documents/2e9e4522-425e-4bd2-9665-d4b5f5f33565.png?token=vhAVX5a_ezZ8CjVDqH6rGolG-qXo4CtYqC3H_mvZc7w&height=750&width=750&expires=33248333306"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="matiasvallejos.com" />
+        <meta property="twitter:url" content="https://matiasvallejos.com/" />
+        <meta name="twitter:title" content="Matias Vallejos - Full Stack Web Developer 🇦🇷" />
+        <meta name="twitter:description" content="Let's explore my personal site! 🚀" />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/documents/2e9e4522-425e-4bd2-9665-d4b5f5f33565.png?token=vhAVX5a_ezZ8CjVDqH6rGolG-qXo4CtYqC3H_mvZc7w&height=750&width=750&expires=33248333306"
+        />
       </head>
       <body className={`relative ${customFont.className} antialiased`}>
         <Providers messages={messages} locale={locale}>
