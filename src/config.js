e00f15c7e0c5f574b07e8b4da0d669f8a3710631
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-04-05 14:32:26 -0300

    Update site name

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports.SITE = {
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'Matias Vallejos - Full Stack Web Developer 🇦🇷',
+  title: 'Matias Vallejos - Full Stack Developer 🇦🇷',
   description: "Let's explore my personal site! 🚀",
   keywords: 'Matias Vallejos, Web Developer, Software Engineer, Argentina, Desafio Planeta, Capitalismo Tecnologico Nacional, React, Next.js, TailwindCSS, Node.js, Express, MongoDB, PostgreSQL, Docker, Kubernetes, AWS, GCP, Azure, CI/CD, DevOps, Agile, Scrum, Kanban, Lean, Design Thinking, UX, UI, SEO, SEM, SMM, SMO, SaaS, PaaS, IaaS, FaaS, DaaS, BaaS, MaaS, XaaS, API, REST, GraphQL, WebSockets, WebRTC, PWA, SPA, SSR, CSR, MPA, AMP, JAMstack, Microservices, Serverless, Headless, Jamstack, Monorepo',
 }
